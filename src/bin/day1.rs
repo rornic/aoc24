a9bf30fa17b40633539245077bb0f469ba95fbb5
Rory N <rorynickolls@gmail.com>
2024-12-02 13:22:11 +0000

    fix: format

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,4 @@
-use aoc24::{Problem, run_problem};
+use aoc24::{run_problem, Problem};
 use regex::Regex;
 
 struct Day1 {}
@@ -22,18 +22,25 @@ impl Problem for Day1 {
         left.sort();
         right.sort();
 
-        let result: u32 = left.iter().zip(right.iter()).map(|(l, r)| l.abs_diff(*r)).sum();
+        let result: u32 = left
+            .iter()
+            .zip(right.iter())
+            .map(|(l, r)| l.abs_diff(*r))
+            .sum();
         format!("{}", result)
     }
 
     fn part2(&self, input: &str) -> String {
         let [left, right] = parse_input(input);
 
-        let similarity_score: u32 = left.iter().map(|num| *num * right.iter().filter(|r| **r == *num).count() as u32).sum();
+        let similarity_score: u32 = left
+            .iter()
+            .map(|num| *num * right.iter().filter(|r| **r == *num).count() as u32)
+            .sum();
         format!("{}", similarity_score)
     }
 }
 
 fn main() {
-    run_problem(Day1{}, "day1");
+    run_problem(Day1 {}, "day1");
 }
--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,21 +1,30 @@
-use aoc24::{Problem, run_problem};
+use aoc24::{run_problem, Problem};
 
 struct Day2 {}
 
 fn parse_input(input: &str) -> Vec<Vec<u32>> {
     let mut reports = Vec::new();
     for line in input.lines() {
-        let levels = line.split(" ").map(|level| level.parse::<u32>().unwrap()).collect();
+        let levels = line
+            .split(" ")
+            .map(|level| level.parse::<u32>().unwrap())
+            .collect();
         reports.push(levels);
     }
     reports
-
 }
 
 fn is_report_safe(report: &[u32]) -> bool {
-    let differences: Vec<i32> = report.iter().zip(report.iter().skip(1)).map(|(a, b)| *b as i32 - *a as i32).collect();
-    differences.iter().all(|diff| diff.abs() >= 1 && diff.abs() <= 3)
-      && (differences.iter().all(|diff| diff.is_negative()) || differences.iter().all(|diff| diff.is_positive()))
+    let differences: Vec<i32> = report
+        .iter()
+        .zip(report.iter().skip(1))
+        .map(|(a, b)| *b as i32 - *a as i32)
+        .collect();
+    differences
+        .iter()
+        .all(|diff| diff.abs() >= 1 && diff.abs() <= 3)
+        && (differences.iter().all(|diff| diff.is_negative())
+            || differences.iter().all(|diff| diff.is_positive()))
 }
 
 fn report_permutations(report: Vec<u32>) -> Vec<Vec<u32>> {
@@ -31,17 +40,24 @@ fn report_permutations(report: Vec<u32>) -> Vec<Vec<u32>> {
 impl Problem for Day2 {
     fn part1(&self, input: &str) -> String {
         let reports = parse_input(input);
-        let safe = reports.into_iter().filter(|report| is_report_safe(report)).count();
+        let safe = reports
+            .into_iter()
+            .filter(|report| is_report_safe(report))
+            .count();
         format!("{}", safe)
     }
 
     fn part2(&self, input: &str) -> String {
         let reports = parse_input(input);
-        let safe = reports.into_iter().map(report_permutations).filter(|reports| reports.iter().any(|report| is_report_safe(report))).count();
+        let safe = reports
+            .into_iter()
+            .map(report_permutations)
+            .filter(|reports| reports.iter().any(|report| is_report_safe(report)))
+            .count();
         format!("{}", safe)
     }
 }
 
 fn main() {
-    run_problem(Day2{}, "day2");
+    run_problem(Day2 {}, "day2");
 }
